17e94f4caf0040e82be2422b3fa38df0aac2b895
Wamuyu <michellewachira25@gmail.com>
2022-06-03 23:19:44 +0300

    commit tree 34438e26186d35d35b1c37dd99911de8028f122f

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b){
+    return a + b;
+}
+   
+add (1514,1);
+
+function subtract(a,b){
+    return a - b;
+}
+subtract (0,187);
+
+function multiply (a,b){
+    return a*b;
+}
+multiply( 45907,4);
+
+function divide (a,b){
+    return a/b;
+}
+divide (7.44715447154,4);
+
+function increment(n){
+return n+=1;
+}
+increment(515);
+
+function decrement (n){
+    return n-=1;
+}
+decrement (396);
+
+function makeInt(n){
+   return parseInt(n,10);
+}
+makeInt (535);
+
+function preserveDecimal(n){
+   return parseFloat(n);
+}
+preserveDecimal(2.222);
